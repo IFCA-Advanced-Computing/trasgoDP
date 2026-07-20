fix: upper and lower bounds for numerical attributes non-optional

tests/test_unitary.py
@@ -22,84 +22,134 @@ class TestAdult(unittest.TestCase):
     def test_error_column_laplace(self):
         column = "educatin"
         epsilon = 1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_laplace(self.data, column, epsilon)
+            numerical.dp_clip_laplace(
+                self.data, column, epsilon, lower_bound, upper_bound
+            )
 
     def test_error_type_column_laplace(self):
         column = "education"
         epsilon = 1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_laplace(self.data, column, epsilon)
+            numerical.dp_clip_laplace(
+                self.data, column, epsilon, lower_bound, upper_bound
+            )
 
     def test_output_laplace(self):
         epsilon = 1
         column = "age"
-        data_dp = numerical.dp_clip_laplace(self.data, column, epsilon)
+        lower_bound = 0
+        upper_bound = 100
+        data_dp = numerical.dp_clip_laplace(
+            self.data, column, epsilon, lower_bound, upper_bound
+        )
         assert isinstance(data_dp, pd.DataFrame)
 
     def test_error_epsilon_laplace(self):
         column = "age"
         epsilon = -1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_laplace(self.data, column, epsilon)
+            numerical.dp_clip_laplace(
+                self.data, column, epsilon, lower_bound, upper_bound
+            )
 
     def test_output_laplace_newcolumn(self):
         epsilon = 1
         column = "age"
-        data_dp = numerical.dp_clip_laplace(self.data, column, epsilon, new_column=True)
+        lower_bound = 0
+        upper_bound = 100
+        data_dp = numerical.dp_clip_laplace(
+            self.data, column, epsilon, lower_bound, upper_bound, new_column=True
+        )
         assert isinstance(data_dp, pd.DataFrame)
 
     def test_output_laplace_newcolumn_len(self):
         epsilon = 1
         column = "age"
-        data_dp = numerical.dp_clip_laplace(self.data, column, epsilon, new_column=True)
+        lower_bound = 0
+        upper_bound = 100
+        data_dp = numerical.dp_clip_laplace(
+            self.data, column, epsilon, lower_bound, upper_bound, new_column=True
+        )
         assert len(data_dp.columns) == len(self.data.columns) + 1
 
     def test_error_column_gaussian(self):
         column = "educatin"
         epsilon = 1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian(self.data, column, epsilon)
+            numerical.dp_clip_gaussian(
+                self.data, column, epsilon, lower_bound, upper_bound
+            )
 
     def test_error_type_column_gaussian(self):
         column = "education"
         epsilon = 1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian(self.data, column, epsilon)
+            numerical.dp_clip_gaussian(
+                self.data, column, epsilon, lower_bound, upper_bound
+            )
 
     def test_error_epsilon_gaussian(self):
         column = "age"
         epsilon = -1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian(self.data, column, epsilon)
+            numerical.dp_clip_gaussian(
+                self.data, column, epsilon, lower_bound, upper_bound
+            )
 
     def test_error_delta_gaussian(self):
         column = "age"
         epsilon = 1
         delta = 1.1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian(self.data, column, epsilon, delta)
+            numerical.dp_clip_gaussian(
+                self.data, column, epsilon, lower_bound, upper_bound, delta
+            )
 
     def test_error_deltaneg_gaussian(self):
         column = "age"
         epsilon = 1
         delta = -1.1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian(self.data, column, epsilon, delta)
+            numerical.dp_clip_gaussian(
+                self.data, column, epsilon, lower_bound, upper_bound, delta
+            )
 
     def test_output_gaussian(self):
         epsilon = 1
         delta = 1e-5
         column = "age"
-        data_dp = numerical.dp_clip_gaussian(self.data, column, epsilon, delta)
+        lower_bound = 0
+        upper_bound = 100
+        data_dp = numerical.dp_clip_gaussian(
+            self.data, column, epsilon, lower_bound, upper_bound, delta
+        )
         assert isinstance(data_dp, pd.DataFrame)
 
     def test_output_gaussian_newcolumn(self):
         epsilon = 1
         delta = 1e-5
         column = "age"
+        lower_bound = 0
+        upper_bound = 100
         data_dp = numerical.dp_clip_gaussian(
-            self.data, column, epsilon, delta, new_column=True
+            self.data, column, epsilon, lower_bound, upper_bound, delta, new_column=True
         )
         assert isinstance(data_dp, pd.DataFrame)
 
@@ -107,8 +157,10 @@ class TestAdult(unittest.TestCase):
         epsilon = 1
         delta = 1e-5
         column = "age"
+        lower_bound = 0
+        upper_bound = 100
         data_dp = numerical.dp_clip_gaussian(
-            self.data, column, epsilon, delta, new_column=True
+            self.data, column, epsilon, lower_bound, upper_bound, delta, new_column=True
         )
         assert len(data_dp.columns) == len(self.data.columns) + 1
 
@@ -398,67 +450,80 @@ class TestAdult(unittest.TestCase):
     def test_laplace_array_epsilon(self):
         data = np.random.rand(100)
         epsilon = -1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_laplace_array(data, epsilon)
+            numerical.dp_clip_laplace_array(data, epsilon, lower_bound, upper_bound)
 
     def test_laplace_array_type(self):
         data = self.data["education"].values
         epsilon = 1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_laplace_array(data, epsilon)
+            numerical.dp_clip_laplace_array(data, epsilon, lower_bound, upper_bound)
 
     def test_laplace_array_output(self):
         data = self.data["age"].values
         epsilon = 1
-        dp_data = numerical.dp_clip_laplace_array(data, epsilon)
+        lower_bound = 0
+        upper_bound = 100
+        dp_data = numerical.dp_clip_laplace_array(
+            data, epsilon, lower_bound, upper_bound
+        )
         assert isinstance(dp_data, np.ndarray)
-
-    def test_laplace_array_range(self):
-        data = self.data["age"].values
-        epsilon = 1
-        dp_data = numerical.dp_clip_laplace_array(data, epsilon)
-        assert max(dp_data) <= max(data) and min(dp_data) >= min(data)
 
     def test_gaussian_array_epsilon(self):
         data = np.random.rand(100)
         epsilon = -1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian_array(data, epsilon)
+            numerical.dp_clip_gaussian_array(data, epsilon, lower_bound, upper_bound)
 
     def test_gaussian_array_delta(self):
         data = np.random.rand(100)
         epsilon = 1
         delta = 2
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian_array(data, epsilon, delta)
+            numerical.dp_clip_gaussian_array(
+                data, epsilon, lower_bound, upper_bound, delta
+            )
 
     def test_gaussian_array_delta_neg(self):
         data = np.random.rand(100)
         epsilon = 1
         delta = -1
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian_array(data, epsilon, delta)
+            numerical.dp_clip_gaussian_array(
+                data, epsilon, lower_bound, upper_bound, delta
+            )
 
     def test_gaussian_array_type(self):
         data = self.data["education"].values
         epsilon = 1
         delta = 1e-5
+        lower_bound = 0
+        upper_bound = 100
         with self.assertRaises(ValueError):
-            numerical.dp_clip_gaussian_array(data, epsilon, delta)
+            numerical.dp_clip_gaussian_array(
+                data, epsilon, lower_bound, upper_bound, delta
+            )
 
     def test_gaussian_array_output(self):
         data = self.data["age"].values
         epsilon = 1
         delta = 1e-5
-        dp_data = numerical.dp_clip_gaussian_array(data, epsilon, delta)
+        lower_bound = 0
+        upper_bound = 100
+        dp_data = numerical.dp_clip_gaussian_array(
+            data, epsilon, lower_bound, upper_bound, delta
+        )
         assert isinstance(dp_data, np.ndarray)
-
-    def test_gaussian_array_range(self):
-        data = self.data["age"].values
-        epsilon = 1
-        delta = 1e-5
-        dp_data = numerical.dp_clip_gaussian_array(data, epsilon, delta)
-        assert max(dp_data) <= max(data) and min(dp_data) >= min(data)
 
     def test_binary_rr_array_epsilon(self):
         data = self.data["sex"].values

trasgodp/numerical/_gaussian.py
@@ -26,18 +26,18 @@ def dp_clip_gaussian(
     df: pd.DataFrame,
     column: str,
     epsilon: float,
-    delta=1e-3,
-    lower_bound=None,
-    upper_bound=None,
-    new_column=False,
+    lower_bound: float,
+    upper_bound: float,
+    delta: float = 1e-3,
+    new_column: bool = False,
 ) -> pd.DataFrame:
     """Apply the Gaussian mechanism to a dataframe numeric column and clip the result.
 
     :param df: dataframe with the data under study.
     :type df: pandas dataframe
 
-    :param columm: column to which the DP mechanism will be applied.
-    :type columm: string
+    :param column: column to which the DP mechanism will be applied.
+    :type column: string
 
     :param epsilon: privacy budget.
     :type epsilon: float
@@ -76,11 +76,6 @@ def dp_clip_gaussian(
     else:
         raise ValueError("Type of the column not allowed for the Gaussian mechanism.")
 
-    if lower_bound is None:
-        lower_bound = min(data.values)
-    if upper_bound is None:
-        upper_bound = max(data.values)
-
     clipped = data.clip(lower_bound, upper_bound)
 
     sensitivity = upper_bound - lower_bound
@@ -103,9 +98,9 @@ def dp_clip_gaussian(
 def dp_clip_gaussian_array(
     data: typing.Union[typing.List, np.ndarray],
     epsilon: float,
-    delta=1e-3,
-    lower_bound=None,
-    upper_bound=None,
+    lower_bound: float,
+    upper_bound: float,
+    delta: float = 1e-3,
 ) -> np.ndarray:
     """Apply the Gaussian mechanism to a list or numpy array.
 
@@ -142,11 +137,6 @@ def dp_clip_gaussian_array(
         data = data.astype(float)
     else:
         raise ValueError("Type of the column not allowed for the Gaussian mechanism.")
-
-    if lower_bound is None:
-        lower_bound = min(data)
-    if upper_bound is None:
-        upper_bound = max(data)
 
     clipped = np.clip(data, lower_bound, upper_bound)
 

trasgodp/numerical/_laplace.py
@@ -26,17 +26,17 @@ def dp_clip_laplace(
     df: pd.DataFrame,
     column: str,
     epsilon: float,
-    lower_bound=None,
-    upper_bound=None,
-    new_column=False,
+    lower_bound: float,
+    upper_bound: float,
+    new_column: bool = False,
 ) -> pd.DataFrame:
     """Apply the Laplace mechanism to a dataframe numeric column and clip the result.
 
     :param df: dataframe with the data under study.
     :type df: pandas dataframe
 
-    :param columm: column to which the DP mechanism will be applied.
-    :type columm: string
+    :param column: column to which the DP mechanism will be applied.
+    :type column: string
 
     :param epsilon: privacy budget.
     :type epsilon: float
@@ -69,11 +69,6 @@ def dp_clip_laplace(
     else:
         raise ValueError("Type of the column not allowed for the Laplace mechanism.")
 
-    if lower_bound is None:
-        lower_bound = min(data.values)
-    if upper_bound is None:
-        upper_bound = max(data.values)
-
     clipped = data.clip(lower_bound, upper_bound)
 
     sensitivity = upper_bound - lower_bound
@@ -97,8 +92,8 @@ def dp_clip_laplace(
 def dp_clip_laplace_array(
     data: typing.Union[typing.List, np.ndarray],
     epsilon: float,
-    lower_bound=None,
-    upper_bound=None,
+    lower_bound: float,
+    upper_bound: float,
 ) -> np.ndarray:
     """Apply the Laplace mechanism to a list or numpy array.
 
@@ -129,11 +124,6 @@ def dp_clip_laplace_array(
         data = data.astype(float)
     else:
         raise ValueError("Type of the column not allowed for the Laplace mechanism.")
-
-    if lower_bound is None:
-        lower_bound = min(data)
-    if upper_bound is None:
-        upper_bound = max(data)
 
     clipped = np.clip(data, lower_bound, upper_bound)
 